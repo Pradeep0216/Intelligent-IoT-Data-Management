913134f93ddb47bdc8c8f2193513ce768e368ed5
Pradeep0216 <s225733102@deakin.edu.au>
2026-09-10 18:58:20 +1000

    Update tester.py
    Signed-off-by: Pradeep0216 <s225733102@deakin.edu.au>

--- data_science/detector_testing/tester.py
+++ data_science/detector_testing/tester.py
@@ -1,1 +1,24 @@
+from data_science.detector_runner import run_detector
 
+
+class DetectorTester:
+
+    def __init__(self, detector_name):
+        self.detector_name = detector_name
+
+    def run_test(self, dataframe, parameters=None):
+        return run_detector(
+            self.detector_name,
+            dataframe,
+            parameters
+        )
+
+    def test_case(self, name, dataframe, parameters=None):
+        result = self.run_test(dataframe, parameters)
+
+        return {
+            "test_name": name,
+            "detector": self.detector_name,
+            "status": result.get("status"),
+            "result": result
+        }

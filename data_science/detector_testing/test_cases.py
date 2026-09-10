34601a6be1862c85aa29628621b3f36724a485b2
Pradeep0216 <s225733102@deakin.edu.au>
2026-09-10 18:58:57 +1000

    Update test_cases.py
    Signed-off-by: Pradeep0216 <s225733102@deakin.edu.au>

--- data_science/detector_testing/test_cases.py
+++ data_science/detector_testing/test_cases.py
@@ -1,1 +1,55 @@
+import numpy as np
+import pandas as pd
 
+
+def normal_data():
+    return pd.DataFrame({
+        "sensor": [10, 11, 10, 12, 11, 10, 12, 11, 10, 11]
+    })
+
+
+def obvious_anomalies():
+    return pd.DataFrame({
+        "sensor": [10, 11, 10, 12, 11, 500, 10, 11, -400, 10]
+    })
+
+
+def no_anomalies():
+    return pd.DataFrame({
+        "sensor": [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+    })
+
+
+def too_few_readings():
+    return pd.DataFrame({
+        "sensor": [10, 11]
+    })
+
+
+def missing_values():
+    return pd.DataFrame({
+        "sensor": [10, 11, np.nan, 12, 13, 11]
+    })
+
+
+def invalid_values():
+    return pd.DataFrame({
+        "sensor": [10, 11, "invalid", 12, 13]
+    })
+
+
+def different_sensor_scales():
+    return pd.DataFrame({
+        "temperature": [20, 21, 20, 22, 21, 20, 23, 21],
+        "pressure": [100000, 100200, 99900, 100100, 100300, 100000, 100400, 100100]
+    })
+
+
+def configuration_data():
+    return pd.DataFrame({
+        "sensor": [
+            10, 11, 10, 12, 11,
+            10, 12, 11, 10, 11,
+            500, -400
+        ]
+    })
